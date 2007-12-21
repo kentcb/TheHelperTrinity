0030fbe3db7837d2a818544c15b8c47bbe7d8113
kentcb <none@none.com>
2007-12-21 21:01:43 +0000

    - checking in initial revision (V1.1.0.0)

---
+++ Src/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("The Helper Trinity Unit Tests")]
+[assembly: AssemblyDescription("Unit tests for The Helper Trinity.")]
+[assembly: AssemblyCompany("Kent Boogaart")]
+[assembly: AssemblyProduct("The Helper Trinity")]
+[assembly: AssemblyCopyright("© Copyright. Kent Boogaart.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
